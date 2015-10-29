5bdc1986a34a60f226859f7189626796aa2407c1
Daniel C. Bucher <daniel.bucher88@gmail.com>
2015-10-28 22:58:43 -0200

    Remove support for JSON format on leads_controller

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -2,13 +2,11 @@ class LeadsController < ApplicationController
   before_action :set_lead, only: [:show, :edit, :update, :destroy]
 
   # GET /leads
-  # GET /leads.json
   def index
     @leads = Lead.all
   end
 
   # GET /leads/1
-  # GET /leads/1.json
   def show
   end
 
@@ -24,55 +22,50 @@ class LeadsController < ApplicationController
   end
 
   # POST /leads
-  # POST /leads.json
   def create
     @lead = Lead.new(lead_params)
 
     respond_to do |format|
       if @lead.save
-        format.html { redirect_to new_lead_path, notice: 'Cadastro realizado com sucesso.' }
-        format.json { render :show, status: :created, location: @lead }
+        format.html { redirect_to new_lead_path,
+          notice: 'Cadastro realizado com sucesso.' }
       else
         format.html { render :new, layout: 'dark_application' }
-        format.json { render json: @lead.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /leads/1
-  # PATCH/PUT /leads/1.json
   def update
     respond_to do |format|
       if @lead.update(lead_params)
         format.html { redirect_to @lead, notice: 'Lead was successfully updated.' }
-        format.json { render :show, status: :ok, location: @lead }
       else
         format.html { render :edit }
-        format.json { render json: @lead.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /leads/1
-  # DELETE /leads/1.json
   def destroy
     @lead.destroy
+
     respond_to do |format|
       format.html { redirect_to leads_url, notice: 'Lead was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_lead
-      @lead = Lead.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def lead_params
-      params.require(:lead)
-        .permit(:name, :email, :mobile_phone, :position_id, :tool,
-                  :team_size, :twitter, :company, :sector)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_lead
+    @lead = Lead.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def lead_params
+    params.require(:lead)
+      .permit(:name, :email, :mobile_phone, :position_id, :tool,
+                :team_size, :twitter, :company, :sector)
+  end
 end
--- app/views/leads/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@leads) do |lead|
-  json.extract! lead, :id, :name, :email, :mobile_phone, :position_id, :tool_id, :team_size
-  json.url lead_url(lead, format: :json)
-end
